c8dc740f49d128ec29e5a1851ed4da9916f53653
jacobartis <ZJAC219@live.rhul.ac.uk>
2023-02-07 14:57:47 +0000

    Created a function to convert a pdf to a dictionary of the charaters stats

--- StatHandler.py
+++ StatHandler.py
@@ -1,7 +1,5 @@
 import PyPDF2
-pdf = open("CumGurglerDickInAss_63856309.pdf","rb")
-pdfReader = PyPDF2.PdfReader(pdf)
-main_page = pdfReader.pages[0]
+
 
 def get_stats(sheet_contents):
     catagories = ["Str","Dex","Con","Int","Wis","Cha"]
@@ -20,17 +18,65 @@ def get_saving_throws(sheet_contents):
     
     return(dict(zip(catagories,stats)),sheet_contents[31])
 
+def get_skills(sheet_contents):
 
-#print(main_page.extract_text())
+    catagories = ["Acrobatics","Animal Handling","Arcana","Athletics","Deception","History","Insight","Intimidation","Investigation","Medicine","Nature","Perception","Performance","Persuasion","Religion","Slight of Hand","Stealth","Survival"]
+    stats = []
+    for x in range(45):
+        if not x%3:
+            stats.append(sheet_contents[x+33])
+    
+    #PDF is scuffed and doesn't have stat for last 3 skills making them every 2
+    for x in range(6):
+        if x%2:
+            stats.append(sheet_contents[x+76])
+    
+    return(dict(zip(catagories,stats)))
 
-stuff = []
+def get_pasive_stats(sheet_contents):
+    catagories = ["Pasive Perception","Pasive Insight","Pasive Investigation"]
+    stats = []
+    for x in range(3):
+        stats.append(sheet_contents[x+82])
+    
+    return(dict(zip(catagories,stats)))
 
-if "/Annots" in main_page:
-    for annot in main_page["/Annots"]:
-        if annot.get_object()["/Subtype"] == "/Widget":
-            obj = annot.get_object()
-            if "/V" in obj:
-                stuff.append(obj["/V"])
+def get_senses(sheet_contents):
+    return sheet_contents[85]
 
-print(get_stats(stuff))
-print(get_saving_throws(stuff))
+def get_initiative(sheet_contents):
+    return sheet_contents[86]
+
+def get_ac(sheet_contents):
+    return sheet_contents[87]
+
+def get_speed(sheet_contents):
+    return sheet_contents[89]
+
+#Converts a given pdf file to dictionary of the characters stats
+def get_char_from_pdf(pdf):
+    catagories = ["Stats", "Saving Throws", "Skills", "Pasive Stats","Senses","Initiative","AC","Speed"]
+    stats = []
+    try:
+        pdf = open(pdf,"rb")
+        pdfReader = PyPDF2.PdfReader(pdf)
+        main_page = pdfReader.pages[0]
+        sheet_info = []
+        for annot in main_page["/Annots"]:
+            if annot.get_object()["/Subtype"] == "/Widget":
+                obj = annot.get_object()
+                if "/V" in obj:
+                    sheet_info.append(obj["/V"])
+        stats.append(get_stats(sheet_info))
+        stats.append(get_saving_throws(sheet_info))
+        stats.append(get_skills(sheet_info))
+        stats.append(get_pasive_stats(sheet_info))
+        stats.append(get_senses(sheet_info))
+        stats.append(get_initiative(sheet_info))
+        stats.append(get_ac(sheet_info))
+        stats.append(get_speed(sheet_info))
+    except Exception as e:
+        print("Something went wrong")
+        print(str(e))
+    
+    return dict(zip(catagories,stats))
