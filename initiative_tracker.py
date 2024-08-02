a6ff5fa1df51c078755dc9f73068bd5a3dc3ce53
jacobartis <ZJAC219@live.rhul.ac.uk>
2024-08-02 21:10:12 +0100

    Privated functions and vars of initiative tracker. Added test to main.

--- initiative_tracker.py
+++ initiative_tracker.py
@@ -21,75 +21,84 @@ class intiative_tracker:
     CREATURE_ID:int = 0
     CREATURE:int = 1
 
-    next_id = 0
-    initiative: int = 0
-    creatures: dict = {
-    }
+    _next_id = 0
+    _initiative: int = 0
+    _creatures: dict = {}
 
     #Stores upcomming creatures
-    initiative_stack:list[initiative_creature] = []
+    _initiative_stack:list[initiative_creature] = []
     #Stores prev creatures
-    initiative_stack_prev:list[initiative_creature] = []
+    _initiative_stack_prev:list[initiative_creature] = []
 
-    def get_next_id(self):
-        id = self.next_id
-        self.next_id += 1
+    def get_creatures(self):
+        return self._creatures
+
+    def get_initiative(self):
+        return self._initiative
+
+    def _get_next_id(self):
+        id = self._next_id
+        self._next_id += 1
         return id 
 
-    def next_turn(self):
-        self.generate_stack()
-        self.initiative = self.initiative_stack[0][self.CREATURE].get_initiative()
-
-    
-    def next_initiative(self):
-        #Resets if empty or last creature finished their turn
-        if len(self.initiative_stack)<= 1:
-            self.next_turn()
-        #Adds current latest creature to the front of the prev stack
-        self.initiative_stack_prev.insert(0,self.initiative_stack.pop(0))
-
-        #Sets current initiative to next turns initiative
-        self.initiative = self.initiative_stack[0][self.CREATURE].get_initiative()
-        print(self.initiative)
-
-    def prev_initiative(self):
-        #Returns if at start of initiative
-        if not len(self.initiative_stack_prev):
-            return
-        
-        #Readds top of prev stack to current stack
-        self.initiative_stack.insert(0,self.initiative_stack_prev.pop(0))
-
-        #Updates initiative to current turn
-        self.initiative = self.initiative_stack[0][self.CREATURE].get_initiative()
-        print(self.initiative)
+    #Updates initiative to current turn
+    def _update_initiative(self):
+        self._initiative = self._initiative_stack[0][self.CREATURE].get_initiative()
 
     #Adds a new creature to the list
     def add_creature(self,initia:int):
-        new_creature = initiative_creature(self.get_next_id(),initia)
-        self.creatures[new_creature.get_id()] = new_creature
-
+        new_creature = initiative_creature(self._get_next_id(),initia)
+        self._creatures[new_creature.get_id()] = new_creature
+    
     #Generates the initiative stack using the current creature list
-    def generate_stack(self, current=0):
+    def _generate_stack(self, current=0):
         #Sorts creatures by initiative high to low and adds them to the stack
-        creature_list = list(self.creatures.items())
+        creature_list = list(self._creatures.items())
         creature_list.sort(key=lambda x:x[self.CREATURE].get_initiative())
         creature_list.reverse()
-        self.initiative_stack = creature_list
-        self.initiative_stack_prev = []
+        self._initiative_stack = creature_list
+        self._initiative_stack_prev = []
+
+    def next_turn(self):
+        self._generate_stack()
+        self._update_initiative()
+    
+    def next_initiative(self):
+        #Resets if empty or last creature finished their turn
+        if len(self._initiative_stack)<= 1:
+            self.next_turn()
+        #Adds current latest creature to the front of the prev stack
+        self._initiative_stack_prev.insert(0,self._initiative_stack.pop(0))
+
+        self._update_initiative()
+        print(self._initiative)
+
+    def prev_initiative(self):
+        #Returns if at start of initiative
+        if not len(self._initiative_stack_prev):
+            return
+        
+        #Readds top of prev stack to current stack
+        self._initiative_stack.insert(0,self._initiative_stack_prev.pop(0))
+
+        self._update_initiative()
+
+        print(self._initiative)
 
 
+def main():
+    tracker = intiative_tracker()
+    for x in range(5):
+        tracker.add_creature(rnd.randint(0,20))
+    tracker.next_turn()
 
+    tracker.next_initiative()
+    tracker.next_initiative()
+    tracker.next_initiative()
+    tracker.prev_initiative()
+    tracker.next_initiative()
+    tracker.next_initiative()
+    tracker.next_initiative()
 
-tracker = intiative_tracker()
-for x in range(5):
-    tracker.add_creature(rnd.randint(0,20))
-tracker.generate_stack()
-
-tracker.next_initiative()
-tracker.next_initiative()
-tracker.next_initiative()
-tracker.prev_initiative()
-tracker.next_initiative()
-tracker.next_initiative()
-tracker.next_initiative()
+if __name__ == "__main__":
+    main()
